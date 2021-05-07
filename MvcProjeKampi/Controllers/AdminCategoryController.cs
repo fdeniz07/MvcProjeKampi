ed29ebf30e49b80e69e81a129aaafa72cc3511f8
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-07 18:04:21 +0200

    36.Ders Sonu

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System.Web.Mvc;
+using BusinessLayer.ValidationRules;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal()); //Ileride Enttiy Framework den
+                                                                                    //vazgecilirse burasi degistirilebilir
+        
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate((category));
+            if (results.IsValid)
+            {
+                categoryManager.CategoryAdd((category));
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                        ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
