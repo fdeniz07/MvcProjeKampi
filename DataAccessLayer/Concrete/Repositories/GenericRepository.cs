a5de9bf4b5ba9269e1a555e704f74c847def1ce8
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-14 23:07:28 +0200

    44.Ders sonu

--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -20,7 +20,9 @@ namespace DataAccessLayer.Concrete.Repositories
 
         public void Delete(T entity)
         {
-            _object.Remove(entity);
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            // _object.Remove(entity);
             context.SaveChanges();
         }
 
@@ -31,7 +33,9 @@ namespace DataAccessLayer.Concrete.Repositories
 
         public void Insert(T entity)
         {
-            _object.Add(entity);
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            //_object.Add(entity);
             context.SaveChanges();
         }
 
@@ -47,6 +51,8 @@ namespace DataAccessLayer.Concrete.Repositories
 
         public void Update(T entity)
         {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
             context.SaveChanges();
         }
     }
