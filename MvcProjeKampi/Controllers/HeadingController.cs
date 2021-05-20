124d047546ab7149e77ac77b786d09edd4495835
Fatih Deniz <deniz.fatih@hotmail.de>
2021-05-20 19:59:42 +0200

    48.Ders Sonu

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,5 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace MvcProjeKampi.Controllers
@@ -8,11 +12,41 @@ namespace MvcProjeKampi.Controllers
     {
         // GET: Heading
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
 
         public ActionResult Index()
         {
             var headingValues = headingManager.GetList();
             return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> _valueCategory = (from x in categoryManager.GetList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryId.ToString()
+                                                   }).ToList();
+            List<SelectListItem> _valueWriter = (from x in writerManager.GetList()
+                                                 select new SelectListItem()
+                                                 {
+                                                     Text = x.WriterName + " " + x.WriterSurName,
+                                                     Value = x.WriterId.ToString()
+                                                 }).ToList();
+            ViewBag.valueCategory = _valueCategory;
+            ViewBag.valueWriter = _valueWriter;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headingManager.HeadingAdd(heading);
+            return RedirectToAction("Index");
         }
     }
 }
