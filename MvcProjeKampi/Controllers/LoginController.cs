20b8020ea0c7f1f677e316dfc52375c241745595
Fatih Deniz <deniz.fatih@hotmail.de>
2021-06-12 03:40:01 +0200

    70.Ders Sonu

--- MvcProjeKampi/Controllers/LoginController.cs
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -1,12 +1,33 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
 
 namespace MvcProjeKampi.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            Context context = new Context();
+            var adminUser = context.Admins.FirstOrDefault(x =>
+                x.AdminUserName == admin.AdminUserName && x.AdminPassword == admin.AdminPassword);
+
+            if (adminUser!=null)
+            {
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
